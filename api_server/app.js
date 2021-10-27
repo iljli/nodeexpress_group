092979599b15e423b245a7ed0974ab9ed174194a
Micha <micha.pdm@gmx.de>
2021-10-27 17:00:56 +0200

    structure: renamed folder, prepare for react

--- api_server/app.js
+++ api_server/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const travelRouter = require('./modules');
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'public')));
+
+
 app.get('/', (req, res) => {
-    res.send(`Welcome to your Recepies-Blog API`)
+    // res.send(`Welcome to your Recepies-Blog API`)
+    res.sendFile(path.join(__dirname, 'HTML', 'index.html'))
 })
 
 app.use('/api/articles', travelRouter);
