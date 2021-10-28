61ac728c50079bb2f4475104c3138da249542a2b
Micha <micha.pdm@gmx.de>
2021-10-28 20:02:45 +0200

    minor changes

--- recepiesblog/src/components/Post.js
+++ recepiesblog/src/components/Post.js
@@ -12,14 +12,17 @@ const Post = ({ text, title, id, headline }) => {
     // console.log(id);
 
     return (
-         <div className={'post'} id={id}>
-             <h2 className={'title'}>{title}</h2>
-             <h3 className={'headline'}>{headline}</h3>
-            {/* {featureImage && <img className='featureImage' src={featureImage.fields.file.url} alt={name} title={name} />} */}
-            {text}
-             <HashLink className="linkToArticle" smooth to='articles#top'>
-                 <button> Go Up</button>
-             </HashLink>
+        <div>
+            <div className={'post'} id={id}>
+                <h2 className={'title'}>{title}</h2>
+                <h3 className={'headline'}>{headline}</h3>
+                {/* {featureImage && <img className='featureImage' src={featureImage.fields.file.url} alt={name} title={name} />} */}
+                {text}
+            </div>
+
+            <HashLink className="linkToArticle" smooth to='articles#top'>
+                <button> Go Up</button>
+            </HashLink>
         </div>
     )
 }
